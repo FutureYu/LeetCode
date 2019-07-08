2550be508291217a331750d2383f3cad34dac31e
FanYu <32595791+FutureYu@users.noreply.github.com>
2019-07-08 11:38:44 +0800

    05-2

--- 05.py
+++ 05.py
@@ -1,35 +1,33 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        size = len(s)
+        if size == 0:
+            return s
         p = []
-        for k in range(len(s)):
-            p.append([False] * len(s))
-        # 先初始化长度为 0 时的结果
-        for k in range(1, len(s)):
-            p[k][k - 1] = True
-        for k in range(len(s)):
-            # 最多只有 len(s) 次遍历，第 k 次遍历长度为 k + 1 的字符串
-            # p(i, i + k) = p (i + 1, i + k - 1) && s(i) == s(i + k)
-            # k 为 0 时，直接初始化为 True
-            for i in range(0, len(s) - k):
-                if k == 0:
-                    p[i][i + k] = True
-                else:
-                    p[i][i + k] = p[i + 1][i + k - 1] and s[i] == s[i + k]
-
-        # 找出最长回文子序列
-        maxi = 0
-        maxj = 0
+        res = s[0]
         maxLen = -1
-        for i in range(len(s)):
-            for j in reversed(range(len(s))):
-                if p[i][j] == True and j - i >= maxLen:
-                    maxLen = j - i
-                    maxi = i
-                    maxj = j
-                    break
-        return s[maxi: maxj + 1]
+        for k in range(size):
+            p.append([-1] * size)
+        # 先初始化长度为 0 时的结果
+        for k in range(1, size):
+            p[k][k - 1] = 0
+        for k in range(size):
+            # 最多只有 size 次遍历，第 k 次遍历长度为 k + 1 的字符串
+            # p 存放长度
+            # p(i, i + k) = p (i + 1, i + k - 1) && s(i) == s(i + k)
+            # k 为 0 时，直接初始化为 1
+            for i in range(0, size - k):
+                if k == 0:
+                    p[i][i + k] = 1
+                else:
+                    if p[i + 1][i + k - 1] != -1 and s[i] == s[i + k]:
+                        p[i][i + k] = p[i + 1][i + k - 1] + 1
+                        if maxLen <= p[i + 1][i + k - 1] + 1:
+                            maxLen = p[i + 1][i + k - 1] + 1
+                            res = s[i: i + k + 1]
+                    else:
+                        p[i][i + k] = -1
+        return res
 
 if __name__ == "__main__":
-    print(Solution.longestPalindrome(Solution, "1111"))
-
-
+    print(Solution.longestPalindrome(Solution, "asasdas"))
